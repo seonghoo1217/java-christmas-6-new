699cc99a9bf92540383e2e786d817df64158b244
seonghoo1217 <seonghoo1217@naver.com>
2023-11-11 15:00:25 +0900

    feat: 메뉴 목록 초기화 및 관리 객체 생성

---
+++ src/main/java/christmas/domain/menu/MenuManager.java
@@ -0,0 +1,25 @@
+package christmas.domain.menu;
+
+import christmas.domain.menu.property.MenuProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuManager {
+
+    private List<Menu> menus = new ArrayList<>();
+
+    public MenuManager() {
+        initializeRestaurantMenu();
+    }
+
+    private void addMenu(Menu menu) {
+        menus.add(menu);
+    }
+
+    private void initializeRestaurantMenu() {
+        for (MenuProperty menuProperty : MenuProperty.values()) {
+            addMenu(new Menu(menuProperty.menuName, menuProperty.menuCost, menuProperty.menuType));
+        }
+    }
+}

2087641f09400ccf3f1118d848e6d92c865ad4ce
seonghoo1217 <seonghoo1217@naver.com>
2023-11-11 23:30:01 +0900

    feat: 캘린더 저장 시 요일 저장 로직 추가 및 필드 값 추가

--- src/main/java/christmas/domain/calender/Calendar.java
+++ src/main/java/christmas/domain/calender/Calendar.java
@@ -2,11 +2,16 @@ package christmas.domain.calender;
 
 import christmas.validation.CalendarValidation;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 public class Calendar {
     private Integer date;
+    private DayOfWeek dayOfWeek;
 
-    public Calendar(String inputDate) {
+    public Calendar(String inputDate, int year, int month) {
         this.date = parsingDateFormat(inputDate);
+        this.dayOfWeek = caclulateDayOfWeek(year, month, this.date);
     }
 
     private Integer parsingDateFormat(String inputDate) {
@@ -17,5 +22,17 @@ public class Calendar {
     private void validate(String inputDate) {
         CalendarValidation calendarValidation = new CalendarValidation();
         calendarValidation.verifyForCalendarCorrectRange(inputDate);
+    }
+
+    private DayOfWeek caclulateDayOfWeek(int year, int month, int day) {
+        return LocalDate.of(year, month, day).getDayOfWeek();
+    }
+
+    public Integer getDate() {
+        return date;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
     }
 }

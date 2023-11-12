421bbc1d1df20c72b294eb89ae2e5c0dc7088c73
seonghoo1217 <seonghoo1217@naver.com>
2023-11-13 02:53:10 +0900

    refactor: Order 클래스 record 타입으로 변경

--- src/main/java/christmas/domain/menu/RestaurantManager.java
+++ src/main/java/christmas/domain/menu/RestaurantManager.java
@@ -2,6 +2,7 @@ package christmas.domain.menu;
 
 import christmas.domain.menu.property.MenuProperty;
 import christmas.domain.order.Order;
+import christmas.tool.MenuExtractTool;
 import christmas.validation.MenuValidation;
 
 import java.util.ArrayList;
@@ -12,10 +13,12 @@ public class RestaurantManager {
     private static final List<Menu> menus = new ArrayList<>();
     private final Order order;
 
+    private static final MenuExtractTool menuExtractTool = new MenuExtractTool();
+
     public RestaurantManager(String orderMenus) {
         initializeRestaurantMenu();
         validate(orderMenus);
-        order = new Order(orderMenus);
+        order = new Order(menuExtractTool.extractOrderStatus(orderMenus), menuExtractTool.extractTotalAmount(orderMenus));
     }
 
     public void addMenu(Menu menu) {
--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,35 +1,7 @@
 package christmas.domain.order;
 
-import christmas.tool.MenuExtractTool;
-
-import java.util.Collections;
 import java.util.Map;
 
-public class Order {
-    private final Map<String, Integer> orderStatus;
-    private final Integer totalAmount;
+public record Order(Map<String, Integer> orderStatus, Integer totalAmount) {
 
-    private static final MenuExtractTool menuExtractTool = new MenuExtractTool();
-
-
-    public Order(String orderMenus) {
-        this.orderStatus = parsingOrderStatus(orderMenus);
-        this.totalAmount = parsingTotalAmount(orderMenus);
-    }
-
-    private Map<String, Integer> parsingOrderStatus(String orderMenus) {
-        return menuExtractTool.extractOrderStatus(orderMenus);
-    }
-
-    private Integer parsingTotalAmount(String orderMenus) {
-        return menuExtractTool.extractTotalAmount(orderMenus);
-    }
-
-    public Map<String, Integer> getOrderStatus() {
-        return Collections.unmodifiableMap(orderStatus);
-    }
-
-    public Integer getTotalAmount() {
-        return totalAmount;
-    }
 }

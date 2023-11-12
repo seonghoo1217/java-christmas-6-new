1b002a0760a25849988a4c78e006336096d82d54
seonghoo1217 <seonghoo1217@naver.com>
2023-11-12 17:51:13 +0900

    feat: Menu 입력 출력문 정적 데이터 추가

--- src/main/java/christmas/view/property/InputProperty.java
+++ src/main/java/christmas/view/property/InputProperty.java
@@ -6,4 +6,5 @@ public class InputProperty {
     public static final Integer MONTH = 12;
     public static final String RESTAURANT_OPEN = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" +
             "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String ORDER_MENUS = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 }

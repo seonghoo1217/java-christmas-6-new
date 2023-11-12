a5e44e184f923118e9c259a0c8588475a447d809
seonghoo1217 <seonghoo1217@naver.com>
2023-11-12 17:02:07 +0900

    feat: 메뉴 중복 주문 유효성 검사 로직 작성

--- src/main/java/christmas/property/ErrorProperty.java
+++ src/main/java/christmas/property/ErrorProperty.java
@@ -5,4 +5,5 @@ public class ErrorProperty {
     public static final String ERROR_INVALIDATE_DATE_RANGE = errorPrefix + "날짜는 1 ~ 31 사이의 값이 입력 되어야 합니다. ";
     public static final String ERROR_INVALIDATE_DATE_NOT_NUMERIC = errorPrefix + "날짜는 숫자 형태의 값외에는 허용하지 않습니다";
     public static final String ERROR_ORDER_NOT_CORRECT_FORMAT = errorPrefix + "주문 양식은 [주문이름]-[개수]이며, 주문을 여러개 할 경우 콤마(,)를 사용하셔야 합니다.";
+    public static final String ERROR_ORDER_IS_DUPLICATE = errorPrefix + "주문시 메뉴가 중복되어서는 안됩니다.";
 }
--- src/main/java/christmas/validation/MenuValidation.java
+++ src/main/java/christmas/validation/MenuValidation.java
@@ -1,19 +1,29 @@
 package christmas.validation;
 
-import christmas.property.ErrorProperty;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import static christmas.property.ErrorProperty.ERROR_ORDER_IS_DUPLICATE;
+import static christmas.property.ErrorProperty.ERROR_ORDER_NOT_CORRECT_FORMAT;
 
 public class MenuValidation {
 
     public static void verifyForMenus(String menus) {
         verifyForOrderCorrectFormat(menus);
+        verifyForOrderMenuDuplicate(menus);
     }
 
     static void verifyForOrderCorrectFormat(String menus) {
         if (!menus.matches("^([가-힣a-zA-Z0-9]+-\\d+)(,[가-힣a-zA-Z0-9]+-\\d+)*$")) {
-            throw new IllegalArgumentException(ErrorProperty.ERROR_ORDER_NOT_CORRECT_FORMAT);
+            throw new IllegalArgumentException(ERROR_ORDER_NOT_CORRECT_FORMAT);
+        }
+    }
+
+    static void verifyForOrderMenuDuplicate(String orderMenus) {
+        List<String> menuNames = extractMenuNames(orderMenus);
+        if (menuNames.size() != new HashSet<>(menuNames).size()) {
+            throw new IllegalArgumentException(ERROR_ORDER_IS_DUPLICATE);
         }
     }
 

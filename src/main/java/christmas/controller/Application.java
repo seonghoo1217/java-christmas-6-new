e2a10405d3039304376889dec11385fe6f349ad1
seonghoo1217 <seonghoo1217@naver.com>
2023-11-12 17:54:01 +0900

    refactor: Application에서 MainController 생성시 inputView,outputView 전달

--- src/main/java/christmas/controller/Application.java
+++ src/main/java/christmas/controller/Application.java
@@ -1,8 +1,19 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        MainController mainController = new MainController();
+        MainController mainController = new MainController(inputView(), outputView());
         mainController.restaurantOpening();
+    }
+
+    private static InputView inputView() {
+        return new InputView();
+    }
+
+    private static OutputView outputView() {
+        return new OutputView();
     }
 }

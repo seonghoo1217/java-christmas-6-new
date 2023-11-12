bc476e3e26aac28bdde95636471be20290d3f676
seonghoo1217 <seonghoo1217@naver.com>
2023-11-12 17:50:52 +0900

    feat: Menu 입력 메서드 추가 및 static 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,18 +2,29 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.domain.calender.Calendar;
+import christmas.domain.menu.MenuManager;
 
 import static christmas.view.property.InputProperty.*;
 
 public class InputView {
 
-    public static Calendar readVisitDate() {
+    public Calendar readVisitDate() {
         System.out.println(RESTAURANT_OPEN);
         try {
             return new Calendar(Console.readLine(), YEAR, MONTH);
         } catch (IllegalArgumentException e) {
             OutputView.outputForInputErrorMessage(e);
             return readVisitDate();
+        }
+    }
+
+    public MenuManager readOrderMenu() {
+        System.out.println(ORDER_MENUS);
+        try {
+            return new MenuManager(Console.readLine());
+        } catch (IllegalArgumentException e) {
+            OutputView.outputForInputErrorMessage(e);
+            return readOrderMenu();
         }
     }
 }

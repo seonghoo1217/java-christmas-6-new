b704d20297b444dbba41ddf8c36af23af37fbcdc
seonghoo1217 <seonghoo1217@naver.com>
2023-11-14 19:41:02 +0900

    feat: 문자열 처리 객체 생성

---
+++ src/main/java/christmas/tool/StringFormatTool.java
@@ -0,0 +1,13 @@
+package christmas.tool;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class StringFormatTool {
+
+    public static String parsingCostFormatWon(Integer cost) {
+        return NumberFormat.getCurrencyInstance(Locale.KOREA).format(cost)
+                .replace("₩", "")
+                .concat("원");
+    }
+}

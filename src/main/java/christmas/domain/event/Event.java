a852c8b021fa2663fd7e8f85dad65a7d092a7114
seonghoo1217 <seonghoo1217@naver.com>
2023-11-14 16:07:28 +0900

    refactor: Event 클래스 enum 타입으로 변경

--- src/main/java/christmas/domain/event/Event.java
+++ src/main/java/christmas/domain/event/Event.java
@@ -1,6 +1,16 @@
 package christmas.domain.event;
 
-public class Event {
-    private Integer promotionPrice;
-    private String promotionContents;
+import static christmas.domain.event.property.PromotionProperty.PRESENTATION_CONTENTS;
+import static christmas.domain.event.property.PromotionProperty.PRESENTATION_PRICE;
+
+public enum Event {
+    PRESENTATION(PRESENTATION_PRICE, PRESENTATION_CONTENTS);
+
+    private final Integer promotionPrice;
+    private final String promotionContetns;
+
+    Event(Integer promotionPrice, String promotionContetns) {
+        this.promotionPrice = promotionPrice;
+        this.promotionContetns = promotionContetns;
+    }
 }

51878a4d7b7f9ce2eb044f909fc34368b184b6bd
seonghoo1217 <seonghoo1217@naver.com>
2023-11-14 15:40:08 +0900

    feat: 출력 static한 요소들 작성

--- src/main/java/christmas/view/property/OutputProperty.java
+++ src/main/java/christmas/view/property/OutputProperty.java
@@ -4,4 +4,9 @@ public class OutputProperty {
     public static final String EVENT_NOTICE = "12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     public static final String ORDER_MENUS = "<주문 메뉴>";
     public static final String TOTAL_AMOUNT = "<할인 전 총주문 금액>";
+    public static final String PRESENT_MENU = "<증정 메뉴>";
+    public static final String PROMOTION_DETAILS = "<혜택 내역>";
+    public static final String PROMOTION_AMOUNT = "<총혜택 금액>";
+    public static final String AFTER_PAYMENT_AMOUNT = "<할인 후 예상 결제 금액>";
+    public static final String BADGE = "<12월 이벤트 배지>";
 }

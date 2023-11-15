3592d99f1594e0ca9ed40bab5a119ef0e320c10e
seonghoo1217 <seonghoo1217@naver.com>
2023-11-15 21:44:57 +0900

    test: 이벤트 대상자 Policy 로직 테스트

---
+++ src/test/java/christmas/policy/EventPolicyTest.java
@@ -0,0 +1,26 @@
+package christmas.policy;
+
+import christmas.core.EventCheckPolicy;
+import christmas.core.EventPolicy;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventPolicyTest {
+
+    @Test
+    void EventPolicy_이벤트_대상자_10000원이상구매_테스트() {
+        //given
+        Integer totalAmount = 10000;
+
+        //when
+        boolean eventTarget = eventPolicy().orderIsEventTarget(totalAmount);
+
+        //then
+        assertThat(eventTarget).isTrue();
+    }
+
+    private EventPolicy eventPolicy() {
+        return new EventCheckPolicy();
+    }
+}

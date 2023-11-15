5179f651595199abcbe582007f64cb51dbff8960
seonghoo1217 <seonghoo1217@naver.com>
2023-11-15 17:52:01 +0900

    test: calendar 객체 유효성 검사 시 1~31 사이 값이 아닌 경우 예외 테스트

---
+++ src/test/java/christmas/validation/CalendarValidationTest.java
@@ -0,0 +1,27 @@
+package christmas.validation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CalendarValidationTest {
+
+    public static CalendarValidation calendarValidation;
+
+    @BeforeEach
+    public void initializeDependency() {
+        calendarValidation = new CalendarValidation();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"32", "0", "-1", "999"})
+    void 날짜_입력_값_1에서_31_사이의_값이_아닌_경우(String inputDate) {
+        //when && then
+        assertThatThrownBy(() -> {
+                    calendarValidation.verifyForCalendarCorrectRange(inputDate);
+                }
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+}

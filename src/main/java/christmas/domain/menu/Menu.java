fbb086a48cf1851a8a2dcc9bd85beb71cb32a85c
Lee.t.c <lsh6451217@gmail.com>
2023-12-05 20:23:05 +0900

    refactor: 메뉴 클래스 필드명 목적에 맞게 변경

--- src/main/java/christmas/domain/menu/Menu.java
+++ src/main/java/christmas/domain/menu/Menu.java
@@ -1,7 +1,5 @@
 package christmas.domain.menu;
 
-import java.util.concurrent.ConcurrentHashMap;
-
-public record Menu(String menu, Integer cost, MenuType menuType) {
+public record Menu(String name, Integer cost, MenuType menuType) {
 
 }

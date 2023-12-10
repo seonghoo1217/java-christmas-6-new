4202de4fd739b249adc79243b798dd8ec7ebed67
Lee.t.c <lsh6451217@gmail.com>
2023-12-11 00:38:37 +0900

    feat: 날짜 범위 유효성 검사 로직 작성

--- src/main/java/christmas/validation/DateValidation.java
+++ src/main/java/christmas/validation/DateValidation.java
@@ -2,15 +2,33 @@ package christmas.validation;
 
 import christmas.domain.date.PickDate;
 
+import java.math.BigInteger;
+
 public class DateValidation {
 
     public void isValidate(String date){
         isNotNumeric(date);
+        isNotCorrectlyDate(date);
     }
 
-    private void isNotNumeric(String date){
-        if (!date.equals("-?\\d+")) {
+    private void isNotNumeric(String date) {
+        if (!date.equals("?\\d+")) {
             throw new IllegalArgumentException(ErrorCode.INVALID_DATE_EXCEPTION.message);
         }
+    }
+
+    private void isNotCorrectlyDate(String date){
+        try {
+            int dateInteger = Integer.parseInt(date);
+            if (!dateIsCalendarFormCorrect(dateInteger)){
+                throw new IllegalArgumentException(ErrorCode.INVALID_DATE_EXCEPTION.message);
+            }
+        }catch (NumberFormatException e){
+            throw new IllegalArgumentException(ErrorCode.INVALID_DATE_EXCEPTION.message);
+        }
+    }
+
+    private boolean dateIsCalendarFormCorrect(Integer dateInteger){
+        return dateInteger > 0 && dateInteger < 32;
     }
 }
